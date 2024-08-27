e00a0eaa9e08274d3ca6366650290dd8f282a2fb
ysrael28 <ysrael.m@escolar.ifrn.edu.br>
2024-08-27 11:07:26 -0300

    27.08.2024

--- Meu Game 3D/Assets/script/player.cs
+++ Meu Game 3D/Assets/script/player.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vector3 = UnityEngine.Vector3;
 
 public class player : MonoBehaviour
 {
     public int velocidade = 10;
     public Rigidbody rb;
+    public int forcaPulo = 10;
+
+    public bool noChao = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +20,29 @@ public class player : MonoBehaviour
         TryGetComponent(out rb);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "chão")
+        {
+            noChao = true;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         Debug.Log("update");
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-
         Vector3 direcao = new UnityEngine.Vector3(x, 0, y);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+        
         if (transform.position.y < -5)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
