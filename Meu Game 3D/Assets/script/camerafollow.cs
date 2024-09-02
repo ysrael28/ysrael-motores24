48cf13dd4e51ae4752d87bd0f1f47b9ee26d7540
ysrael28 <ysrael.m@escolar.ifrn.edu.br>
2024-09-02 11:48:25 -0300

    02.09.2024

---
+++ Meu Game 3D/Assets/script/camerafollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    private Transform alvo;
+
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alvo = GameObject.FindWithTag("Player").transform;
+        offset = alvo.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = alvo.position - offset;
+
+    }
+}
