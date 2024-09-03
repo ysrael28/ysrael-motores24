b9a59a80a5f7125b6b2a5da0e55864d774ea6f7a
ysrael28 <ysrael.m@escolar.ifrn.edu.br>
2024-09-03 10:12:43 -0300

    03.09.2024

---
+++ Meu Game 3D/Assets/script/moeda.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moeda : MonoBehaviour
+{
+
+    public int velocidadeGiro = 50;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * velocidadeGiro * Time.deltaTime);
+    }
+}
